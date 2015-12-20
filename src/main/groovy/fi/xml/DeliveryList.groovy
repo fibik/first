6a3780bfbcbd4c43679fa64e01d895e3c157d549
fibik <fibik@ukr.net>
2015-12-21 00:32:44 +0200

    draft

---
+++ src/main/groovy/fi/xml/DeliveryList.groovy
@@ -0,0 +1,40 @@
+package fi.xml
+
+/**
+ * 
+ * List of delivery
+ * 
+ * @author fibik
+ *
+ */
+class DeliveryList {
+
+	/*
+	 * <deliveries>
+	 * 	<delivery>
+	 * 		<name/>
+	 * 		<link/>
+	 * 		<status/>
+	 * 		<environments>
+	 * 			<env name="sit|qa|prod">
+	 * 				<week/>
+	 * 				<date/>
+	 * 				<planned_week/>
+	 * 				<planned_date/>
+	 * 				<status/>
+	 * 				<comment/>
+	 * 			</env>
+	 * 		</environments>
+	 * 		<description/>
+	 * 	</delivery>
+	 * </deliveries>
+	 * 
+	 */
+	
+	// TODO: Tree of directories with Release information
+	// TODO: GUI
+	// TODO: TFS integration
+	// TODO: SVN integration
+	// 
+	
+}
